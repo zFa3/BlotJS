373b6b368e807ca5bdcca1978e3c7e9a30b7fa13
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-08 14:57:27 -0400

    Testing out Blot JS

--- Blot.js
+++ Blot.js
@@ -1,3 +1,10 @@
+/*
+@title: 
+@author: Jayden W
+@snapshot: snapshot1.png
+*/
+
+
 // welcome to blot!
 
 // check out this guide to learn how to program in blot
@@ -6,19 +13,50 @@
 const width = 125;
 const height = 125;
 
-setDocDimensions(width, height);
+// x and y value for the bottom left corner of the card
+const startingCorner = 25
+const rad = 5
+// the ratio between the height and the width
+// of normal playing cards approximates the golden ratio
+const phi = 1.6180339887
 
-// store final lines here
+// width of the card
+// measured in millimeters
+const cardWidth = 64
+// height of the card
+const cardHeight = Number(cardWidth * phi)
+
+// set dimensions
+setDocDimensions(width, height);
+// just push all polylines to this list
 const finalLines = [];
 
 // create a polyline
+/*
 const polyline = [
-  [0, 0],
-  [width * 0.25, height * 0.25]
+  [startingCorner, startingCorner],
+  [startingCorner + cardWidth, startingCorner],
+  [startingCorner + cardWidth, startingCorner + cardHeight],
+  [startingCorner, startingCorner + cardHeight],
+  [startingCorner, startingCorner],
 ];
+*/
+
+const line1 = [
+    [startingCorner + rad, startingCorner],
+    [startingCorner + cardWidth, startingCorner]
+]
+
+const line2 = [
+    [startingCorner + rad, startingCorner + 2 * rad + cardHeight],
+    [startingCorner + cardWidth, startingCorner + 2 * rad + cardHeight]
+]
 
 // add the polyline to the final lines
-finalLines.push(polyline);
+finalLines.push(line1);
+finalLines.push(line2);
+//finalLines.push(line3);
+//finalLines.push(line4);
 
 // transform lines using the toolkit
 // bt.rotate(finalLines, 45);
