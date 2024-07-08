f5bf28339b9c4a30e03265549866eb1bde97a91b
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-08 15:22:29 -0400

    Added nurbs (b-spline curves) curves in the corners
    still testing out the capabilities of Blot
    and the packages that come with it in the online editor

--- Blot.js
+++ Blot.js
@@ -14,17 +14,14 @@ const width = 125;
 const height = 125;
 
 // x and y value for the bottom left corner of the card
-const startingCorner = 25
+const startingCorner = 15
 const rad = 5
-// the ratio between the height and the width
-// of normal playing cards approximates the golden ratio
-const phi = 1.6180339887
 
 // width of the card
 // measured in millimeters
-const cardWidth = 64
+const cardWidth = 43
 // height of the card
-const cardHeight = Number(cardWidth * phi)
+const cardHeight = 59
 
 // set dimensions
 setDocDimensions(width, height);
@@ -52,14 +49,57 @@ const line2 = [
     [startingCorner + cardWidth, startingCorner + 2 * rad + cardHeight]
 ]
 
+const line3 = [
+    [startingCorner, startingCorner + rad],
+    [startingCorner, startingCorner + rad + cardHeight]
+]
+
+const line4 = [
+    [startingCorner + rad + cardWidth, startingCorner + rad],
+    [startingCorner + rad + cardWidth, startingCorner + rad + cardHeight]
+]
+
 // add the polyline to the final lines
+// create the card edges
 finalLines.push(line1);
 finalLines.push(line2);
-//finalLines.push(line3);
-//finalLines.push(line4);
+finalLines.push(line3);
+finalLines.push(line4);
 
 // transform lines using the toolkit
 // bt.rotate(finalLines, 45);
+
+const curve1 = [
+    [startingCorner + rad, startingCorner],
+    [startingCorner, startingCorner],
+    [startingCorner, startingCorner + rad],
+]
+
+finalLines.push(bt.nurbs(curve1))
+
+const curve2 = [
+    [startingCorner + cardWidth, startingCorner],
+    [startingCorner + cardWidth + rad, startingCorner],
+    [startingCorner + cardWidth + rad, startingCorner + rad]
+]
+
+finalLines.push(bt.nurbs(curve2))
+
+const curve3 = [
+    [startingCorner, startingCorner + rad + cardHeight],
+    [startingCorner, startingCorner + 2 * rad + cardHeight],
+    [startingCorner + rad, startingCorner + 2 * rad + cardHeight],
+]
+
+finalLines.push(bt.nurbs(curve3))
+
+const curve4 = [
+    [startingCorner + cardWidth, startingCorner + 2 * rad + cardHeight],
+    [startingCorner + rad + cardWidth, startingCorner + 2 * rad + cardHeight],
+    [startingCorner + rad + cardWidth, startingCorner + rad + cardHeight],
+]
+
+finalLines.push(bt.nurbs(curve4))
 
 // draw it
 drawLines(finalLines);
