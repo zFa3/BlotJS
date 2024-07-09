7a7ae0ad393270a155bd81c31f3eb866df5492c4
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-09 10:27:36 -0400

    added shebang, made it copy pasteable

--- PillowImageConverter.py
+++ PillowImageConverter.py
@@ -1,3 +1,6 @@
+#!usr/bin/env/python 3
+# zFa3 - BlotMesh Generator
+
 '''
 from PIL import Image 
 image_file = Image.open("testimage.jpg") # open
@@ -32,9 +35,15 @@ print(")")
 '''
 
 from PIL import Image 
+print("""
+const width = 125;
+const height = 125;
+
+setDocDimensions(width, height);
+""")
 print("const x = 0\nconst y = 0")
 print("const Poly = []")
-image_file = Image.open("testimage.jpg") # open
+image_file = Image.open("testimage.png") # open
 image_file = image_file.convert("1") # convert image to black and white
 image_file.resize((125, 125)) # resize the image to fit within the bounds
 lines = "const generated_polyline = ["
@@ -47,5 +56,5 @@ lines += "]"
 # image_file.save('result.png') # save result - not required
 print(lines)
 print("Poly.push(generated_polyline)")
-print("bt.scale(Poly), [0.1, 0.1]")
+print("bt.scale((Poly), [0.1, 0.1])")
 print("drawLines(Poly)")
