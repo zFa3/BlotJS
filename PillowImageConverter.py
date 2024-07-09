0c3df487a3532fd4096277e261bb3eff7d2e84b1
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-09 09:54:07 -0400

    Fixed accidental addition of old code

--- PillowImageConverter.py
+++ PillowImageConverter.py
@@ -37,7 +37,7 @@ image_file = Image.open("testimage.jpg") # open
 image_file = image_file.convert("1") # convert image to black and white
 image_file.resize((125, 125)) # resize the image to fit within the bounds
 lines = "generated_polyline = ["
-pixels = list(image_file.load())
+pixels = image_file.load()
 for j in range(image_file.height):
     for i in range(image_file.width):
         if not pixels[i, j]:
