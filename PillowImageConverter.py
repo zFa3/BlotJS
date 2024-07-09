e1047d1ea0b8bacaf64296cc228ff386e9452075
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-09 10:02:31 -0400

    Fixed some bugs, added more print lines

--- PillowImageConverter.py
+++ PillowImageConverter.py
@@ -17,7 +17,7 @@ for j in range(image_file.height):
             lines += f"\nconst line{count} = ["
         if not pixels[i, j]:
             lines += f"[{i}, {j}],[{i}, {j+1}],"
-            if not(i == prev[0] + 1 or i == prev[0] - 1):
+            if not(i == prev[0]+1 or i == prev[0] - 1):
                 lines += "]"
                 new = True
                 count += 1
@@ -26,22 +26,26 @@ lines += "]"
 # image_file.save('result.png') # save result - not required
 print(lines)
 print(f"finalLines.push(", end = "")
-for i in range(1, count + 1):
-    print("line" + str(i) + ", ", end = "")
+for i in range(1, count+1):
+    print("line"+str(i)+", ", end = "")
 print(")")
 '''
 
 from PIL import Image 
-print("offset_x = 0\noffset_y = 0")
+print("const x = 0\nconst y = 0")
+print("const Poly = []")
 image_file = Image.open("testimage.jpg") # open
 image_file = image_file.convert("1") # convert image to black and white
 image_file.resize((125, 125)) # resize the image to fit within the bounds
-lines = "generated_polyline = ["
+lines = "const generated_polyline = ["
 pixels = image_file.load()
 for j in range(image_file.height):
     for i in range(image_file.width):
         if not pixels[i, j]:
-            lines += f"[{i} + offset_x, {j} + offset_y],[{i} + offset_x, {j+1} + offset_y],"
+            lines += f"[{i}+x, {j}+y],[{i}+x, {j+1}+y],"
 lines += "]"
 # image_file.save('result.png') # save result - not required
 print(lines)
+print("Poly.push(generated_polyline)")
+print("bt.scale(Poly), [0.1, 0.1]")
+print("drawLines(Poly)")
