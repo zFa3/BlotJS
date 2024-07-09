e485229fa111c56d7eb52e5ea1f2cc1a6860b69b
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-08 21:32:34 -0400

    Added the swords to the playing card

--- Blot.js
+++ Blot.js
@@ -11,11 +11,11 @@
 // https://blot.hackclub.com/editor?guide=start
 
 const width = 125;
-const height = 125;
+const height = 160;
 
 // x and y value for the bottom left corner of the card
-const startingCorner = 15
-const rad = 5
+const startingCorner = 29
+const rad = 6
 
 // width of the card
 // measured in millimeters
@@ -28,6 +28,10 @@ setDocDimensions(width, height);
 // just push all polylines to this list
 const finalLines = [];
 
+const bladeLen = 2.9 / 10 // measured in cm
+const hiltLength = 1.1 / 10 // measured in cm
+
+
 // create a polyline
 /*
 const polyline = [
@@ -38,6 +42,36 @@ const polyline = [
   [startingCorner, startingCorner],
 ];
 */
+
+
+
+const sword = [
+    // create the blade
+    [startingCorner + 2.25 * rad, startingCorner + 3 * rad + bladeLen * cardHeight],
+    [startingCorner + 2.25 * rad, startingCorner + 3 * rad],
+    [startingCorner + 2 * rad, startingCorner + 2 * rad],
+    [startingCorner + 1.75 * rad, startingCorner + 3 * rad],
+    [startingCorner + 1.75 * rad, startingCorner + 3 * rad + bladeLen * cardHeight],
+]
+
+const hilt = [
+    // create the hilt
+    [startingCorner + 1.5 * rad, startingCorner + 3 * rad + bladeLen * cardHeight],
+    [startingCorner + 2.5 * rad, startingCorner + 3 * rad + bladeLen * cardHeight],
+    [startingCorner + 2.5 * rad, startingCorner + 3 * rad + (bladeLen + 0.02) * cardHeight],
+    [startingCorner + 2.15 * rad, startingCorner + 3 * rad + (bladeLen + 0.02) * cardHeight],
+    [startingCorner + 2.15 * rad, startingCorner + 3 * rad + (bladeLen + hiltLength) * cardHeight],
+    [startingCorner + 1.85 * rad, startingCorner + 3 * rad + (bladeLen + hiltLength) * cardHeight],
+    [startingCorner + 1.85 * rad, startingCorner + 3 * rad + (bladeLen + 0.02) * cardHeight],
+    [startingCorner + 1.5 * rad, startingCorner + 3 * rad + (bladeLen + 0.02) * cardHeight],
+    [startingCorner + 1.5 * rad, startingCorner + 3 * rad + bladeLen * cardHeight],
+]
+
+finalLines.push(sword, hilt)
+
+/////////////////////////////////////////////////////////
+//               Create the Outer Lines                //
+/////////////////////////////////////////////////////////
 
 const line1 = [
     [startingCorner + rad, startingCorner],
@@ -61,13 +95,38 @@ const line4 = [
 
 // add the polyline to the final lines
 // create the card edges
-finalLines.push(line1);
-finalLines.push(line2);
-finalLines.push(line3);
-finalLines.push(line4);
+// Push all the lines onto the finalLines stack
+finalLines.push(line1, line2, line3, line4)
 
-// transform lines using the toolkit
-// bt.rotate(finalLines, 45);
+
+/////////////////////////////////////////////////////////
+//               Create the Inner Lines                //
+/////////////////////////////////////////////////////////
+
+const poly1 = [
+    [startingCorner + rad, startingCorner + rad],
+    [startingCorner + cardWidth, startingCorner + rad]
+]
+
+const poly2 = [
+    [startingCorner + rad, startingCorner + rad + cardHeight],
+    [startingCorner + cardWidth, startingCorner + rad + cardHeight]
+]
+
+const poly3 = [
+    [startingCorner + rad, startingCorner + rad],
+    [startingCorner + rad, startingCorner + cardHeight + rad]
+]
+
+const poly4 = [
+    [startingCorner + cardWidth, startingCorner + rad],
+    [startingCorner + cardWidth, startingCorner + rad + cardHeight]
+]
+
+// add the polyline to the final lines
+// create the card edges
+// Push all the lines onto the finalLines stack
+finalLines.push(poly1, poly2, poly3, poly4)
 
 const curve1 = [
     [startingCorner + rad, startingCorner],
@@ -75,15 +134,11 @@ const curve1 = [
     [startingCorner, startingCorner + rad],
 ]
 
-finalLines.push(bt.nurbs(curve1))
-
 const curve2 = [
     [startingCorner + cardWidth, startingCorner],
     [startingCorner + cardWidth + rad, startingCorner],
     [startingCorner + cardWidth + rad, startingCorner + rad]
 ]
-
-finalLines.push(bt.nurbs(curve2))
 
 const curve3 = [
     [startingCorner, startingCorner + rad + cardHeight],
@@ -91,15 +146,20 @@ const curve3 = [
     [startingCorner + rad, startingCorner + 2 * rad + cardHeight],
 ]
 
-finalLines.push(bt.nurbs(curve3))
-
 const curve4 = [
     [startingCorner + cardWidth, startingCorner + 2 * rad + cardHeight],
     [startingCorner + rad + cardWidth, startingCorner + 2 * rad + cardHeight],
     [startingCorner + rad + cardWidth, startingCorner + rad + cardHeight],
 ]
 
-finalLines.push(bt.nurbs(curve4))
+// Push all the lines onto the finalLines stack
+finalLines.push(bt.nurbs(curve1), bt.nurbs(curve2), bt.nurbs(curve3), bt.nurbs(curve4))
 
-// draw it
+// draw first set
+drawLines(finalLines);
+
+// rotate lines using the toolkit
+bt.rotate(finalLines, 180);
+
+// draw flipped, 
 drawLines(finalLines);
