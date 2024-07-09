afc4068a550a0eb033b6466a784c673d080fd468
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-09 11:19:02 -0400

    Update PillowImageConverter.py

--- PillowImageConverter.py
+++ PillowImageConverter.py
@@ -1,39 +1,6 @@
 #!usr/bin/env/python 3
 # zFa3 - BlotMesh Generator
 
-'''
-from PIL import Image 
-image_file = Image.open("testimage.jpg") # open
-image_file = image_file.convert("1") # convert image to black and white
-image_file.resize((125, 125)) # resize the image to fit within the bounds
-
-new = True
-count = 1
-
-lines = ""
-pixels = image_file.load()
-prev = [-2, -2]
-for j in range(image_file.height):
-    for i in range(image_file.width):
-        if new:
-            new = False
-            lines += f"\nconst line{count} = ["
-        if not pixels[i, j]:
-            lines += f"[{i}, {j}],[{i}, {j+1}],"
-            if not(i == prev[0]+1 or i == prev[0] - 1):
-                lines += "]"
-                new = True
-                count += 1
-            prev = [i, j]
-lines += "]"
-# image_file.save('result.png') # save result - not required
-print(lines)
-print(f"finalLines.push(", end = "")
-for i in range(1, count+1):
-    print("line"+str(i)+", ", end = "")
-print(")")
-'''
-
 from PIL import Image 
 print("""
 const width = 125;
